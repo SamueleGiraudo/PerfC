057d650dd99cdd46a99c23e46f3030e1c66b37c7
Samuele Giraudo <giraudo.samuele@gmail.com>
2016-05-03 11:52:50 +0200

    TD6/E5.c

--- TD/TD6/E5.c
+++ TD/TD6/E5.c
@@ -17,17 +17,25 @@
 
 int main() {
 
+    short tab_entiers[] = {10, 20, 40, 70, 44, 21, 90, 999, 10, 20, 32};
     void *tab;
+    int val;
+    
+    tab = tab_entiers;
 
     /** 4. */
-    ((short *) tab)[3];
-
-    *(tab + (sizeof(short) * 3));
+    val = ((short *) tab)[3];
+    printf("%d\n", val);
+    
+    val = *(((short *) tab) + 3);
+    printf("%d\n", val);
 
     /** 5. */
-    ((int *) tab)[3];
-
-    *(tab + (sizeof(int) * 3));
+    val = ((int *) tab)[3];
+    printf("%d\n", val);
+    
+    val = *(((int *) tab) + 3);
+    printf("%d\n", val);
 
     return 0;
 }
